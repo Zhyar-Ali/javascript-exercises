19cdbda3928fe269131aea570c67e510253ddc93
Zhyar <zhiarali595@gmail.com>
2026-07-19 12:32:30 +0300

    finished 01_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function(number) {
+    if(!isNaN(number)){
+        if(number === 0) {
+            return 1;
+        }else {
+            return number * factorial(number-1);
+        }
+    }
 };
 
 // Do not edit below this line
