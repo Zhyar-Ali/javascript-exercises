1755ded68f9a79d56a30d665cadc78c556e2b985
Zhyar <zhiarali595@gmail.com>
2026-07-19 12:54:34 +0300

    finished 02_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,11 @@
-const contains = function() {
-  
+const contains = function(object, value) {
+    const array = Object.values(object);
+
+    if (array.includes(value)) {return true;}
+
+    const nests = array.filter((value) => typeof value === 'object' && value !== null);
+
+    return nests.some((nest) => contains(nest, value));
 };
   
 // Do not edit below this line
