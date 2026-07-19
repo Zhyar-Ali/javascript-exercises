27b70badf722b87d32f4b4704dccaaa4545eb5f2
Zhyar <zhiarali595@gmail.com>
2026-07-19 19:26:05 +0300

    finish 03 totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,23 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(input) {
+    if(Array.isArray(input) || typeof input === "object") {
+        var arr;
+        var total = 0;
+        if(typeof input === "object") {
+            arr = Object.values(input);
+        }else {
+            arr = input;
+        }
+
+        for(const values of arr) {
+            if(Number.isInteger(values)) {
+                total++;
+            } else if(typeof values === "object" && values !== null) {
+                total += totalIntegers(values);
+            }
+        }
+
+        return total;
+    } 
 };
   
 // Do not edit below this line
